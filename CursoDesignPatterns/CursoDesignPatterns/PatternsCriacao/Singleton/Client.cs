52592d12f55d617864f075cae3a41a39e66fa2ac
Diego Queiros <diegoqueiros@hotmail.com>
2023-02-07 07:07:30 -0300

    Singleton

---
+++ CursoDesignPatterns/CursoDesignPatterns/PatternsCriacao/Singleton/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns.PatternsCriacao.Singleton
+{
+    public class Client
+    {
+        public void ConsumirDB()
+        {
+            var instancia = ContextDB.Instancia;
+            instancia.ExecutaQuery("SELECT * FROM Nome_Tabela;");
+
+            instancia = ContextDB.Instancia;
+            instancia.ExecutaQuery("INSERT INTO Nome_Tabela (valor) VALUES (0001);");
+
+            instancia = ContextDB.Instancia;
+            instancia.ExecutaQuery("UPDATE Nome_Tabela valor=0002 WHERE Id=0001;");
+
+            instancia = ContextDB.Instancia;
+            instancia.ExecutaQuery("DELETE Nome_Tabela WHERE Id=0001;");
+        }
+    }
+}
--- CursoDesignPatterns/CursoDesignPatterns/Program.cs
+++ CursoDesignPatterns/CursoDesignPatterns/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using CursoDesignPatterns.PatternsCriacao.Abstract_Factory;
+using CursoDesignPatterns.PatternsCriacao.Singleton;
 
 Console.WriteLine("\nCurso Design Patterns\n");
 
 Client client = new Client();
-client.ConsultarRotinaAluno();
+client.ConsumirDB();
